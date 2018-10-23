abfcfbc02d62dfc807290a549876a7b0dffbdaf1
kevinli224 <kevin224li@gmail.com>
2018-10-23 15:08:35 -0400

    A2 files cloned to Phase1 folder

---
+++ Phase1/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/Board.java
@@ -0,0 +1,110 @@
+package fall2018.csc2017.slidingtiles;
+
+import java.util.Observable;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * The sliding tiles board.
+ */
+public class Board extends Observable implements Serializable, Iterable<Tile> {
+
+
+    /**
+     * The number of rows.
+     */
+    final static int NUM_ROWS = 4;
+
+    /**
+     * The number of rows.
+     */
+    final static int NUM_COLS = 4;
+
+    /**
+     * The tiles on the board in row-major order.
+     */
+    private Tile[][] tiles = new Tile[NUM_ROWS][NUM_COLS];
+
+    /**
+     * A new board of tiles in row-major order.
+     * Precondition: len(tiles) == NUM_ROWS * NUM_COLS
+     *
+     * @param tiles the tiles for the board
+     */
+    Board(List<Tile> tiles) {
+        Iterator<Tile> iter = tiles.iterator();
+
+        for (int row = 0; row != Board.NUM_ROWS; row++) {
+            for (int col = 0; col != Board.NUM_COLS; col++) {
+                this.tiles[row][col] = iter.next();
+            }
+        }
+    }
+
+    /**
+     * Return the number of tiles on the board.
+     * @return the number of tiles on the board
+     */
+    int numTiles() {
+        return tiles.length * tiles[0].length;
+    }
+
+    /**
+     * Return the tile at (row, col)
+     *
+     * @param row the tile row
+     * @param col the tile column
+     * @return the tile at (row, col)
+     */
+    Tile getTile(int row, int col) {
+        return tiles[row][col];
+    }
+
+    /**
+     * Swap the tiles at (row1, col1) and (row2, col2)
+     *
+     * @param row1 the first tile row
+     * @param col1 the first tile col
+     * @param row2 the second tile row
+     * @param col2 the second tile col
+     */
+    void swapTiles(int row1, int col1, int row2, int col2) {
+        Tile firstTile = getTile(row1, col1);
+        Tile secondTile = getTile(row2, col2);
+        tiles[row1][col1] = secondTile;
+        tiles[row2][col2] = firstTile;
+        setChanged();
+        notifyObservers();
+    }
+
+    @Override
+    public String toString() {
+        return "Board{" +
+                "tiles=" + Arrays.toString(tiles) +
+                '}';
+    }
+    @Override
+    public Iterator<Tile> iterator(){
+        return new Iterator<Tile>() {
+            private int jIndex = 0, kIndex = 0;
+            private int jLength = tiles.length, kLength = tiles[0].length;
+            @Override
+            public boolean hasNext() {
+                return (jLength + kLength - 1) > (jIndex + kIndex);
+            }
+
+            @Override
+            public Tile next() {
+                if(kIndex >= kLength)
+                {
+                    kIndex = 0;
+                    jIndex++;
+                }
+                return tiles[jIndex][kIndex++];
+            }
+        };
+    }
+}
