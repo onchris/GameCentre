e523445d354cb750a82838d5230219bc07c38f98
linshixi <shixiu.lin@mail.utoronto.ca>
2018-10-30 20:14:37 -0400

    Add undo functionality and also undo, save buttons to activity main layout

--- Phase1/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/BoardManager.java
+++ Phase1/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/BoardManager.java
@@ -2,21 +2,41 @@ package fall2018.csc2017.slidingtiles;
 
 import android.util.Log;
 
+import java.io.NotActiveException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * Manage a board, including swapping tiles, checking for a win, and managing taps.
  */
-class BoardManager implements Serializable {
+class BoardManager implements Serializable, Undoable {
 
     /**
      * The board being managed.
      */
     private Board board;
+
+    /**
+     * The number of steps that users can undo.
+     */
+    private int numCanUndo = 3;
+
+    /**
+     * The steps for users to undo.
+     */
+    private Stack<Integer> availableUndoSteps = new Stack<>();
+
+    /**
+     * Gets the available number of steps that users can undo.
+     * @return the available number of steps that users can undo
+     */
+    public int getNumCanUndo() {
+        return numCanUndo;
+    }
 
     /**
      * Manage a board that has been pre-populated.
@@ -99,6 +119,8 @@ class BoardManager implements Serializable {
         int blankId = board.numTiles();
         if(isValidTap(position))
         {
+            availableUndoSteps.push(row);
+            availableUndoSteps.push(col);
             Tile above = row == 0 ? null : board.getTile(row - 1, col);
             Tile below = row == Board.NUM_ROWS - 1 ? null : board.getTile(row + 1, col);
             Tile left = col == 0 ? null : board.getTile(row, col - 1);
@@ -106,19 +128,44 @@ class BoardManager implements Serializable {
             if(above != null && above.getId() == blankId)
             {
                 board.swapTiles(row, col, row-1, col);
+                availableUndoSteps.push(row - 1 );
+                availableUndoSteps.push(col);
             }
             else if(below != null && below.getId() == blankId)
             {
                 board.swapTiles(row, col, row+1, col);
+                availableUndoSteps.push(row + 1);
+                availableUndoSteps.push(col);
             }
             else if(left != null && left.getId() == blankId)
             {
                 board.swapTiles(row, col, row, col-1);
+                availableUndoSteps.push(row);
+                availableUndoSteps.push(col - 1);
             }
             else if(right != null && right.getId() == blankId)
             {
                 board.swapTiles(row, col, row, col+1);
+                availableUndoSteps.push(row);
+                availableUndoSteps.push(col + 1);
             }
+        }
+    }
+
+    @Override
+    public boolean canUndo() {
+        return (!availableUndoSteps.isEmpty() && numCanUndo != 0);
+    }
+
+    @Override
+    public void undo() {
+        if (canUndo()){
+            int col1 = availableUndoSteps.pop();
+            int row1 = availableUndoSteps.pop();
+            int col2 = availableUndoSteps.pop();
+            int row2 = availableUndoSteps.pop();
+            board.swapTiles(row1, col1, row2, col2);
+            numCanUndo --;
         }
     }
 
