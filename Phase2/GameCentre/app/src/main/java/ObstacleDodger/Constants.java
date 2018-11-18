b7aab652e88fcf9199f16a1fa8969e32f19ed331
chrison <chris.on@mail.utoronto.ca>
2018-11-18 04:42:46 -0500

    Added Obstacle Dodger Game

---
+++ Phase2/GameCentre/app/src/main/java/ObstacleDodger/Constants.java
@@ -0,0 +1,15 @@
+package ObstacleDodger;
+
+/*
+Adapted from:
+https://www.youtube.com/watch?v=OojQitoAEXs - Retro Chicken Android Studio 2D Game Series
+ */
+
+import android.content.Context;
+
+public class Constants {
+    public static int SCREEN_WIDTH;
+    public static int SCREEN_HEIGHT;
+
+    public static Context CURRENT_CONTEXT;
+}
---
+++ Phase2/GameCentre/app/src/main/java/ObstacleDodger/SceneManager.java
@@ -0,0 +1,33 @@
+package ObstacleDodger;
+
+/*
+Adapted from:
+https://www.youtube.com/watch?v=OojQitoAEXs - Retro Chicken Android Studio 2D Game Series
+ */
+
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+
+import java.util.ArrayList;
+
+public class SceneManager {
+    private ArrayList<Scene> scenes = new ArrayList<>();
+    public static int ACTIVE_SCENE;
+
+    public SceneManager() {
+        ACTIVE_SCENE = 0;
+        scenes.add(new GameplayScene());
+    }
+
+    public void receiveTouch(MotionEvent event) {
+        scenes.get(ACTIVE_SCENE).receiveTouch(event);
+    }
+
+    public void update() {
+        scenes.get(ACTIVE_SCENE).update();
+    }
+
+    public void draw(Canvas canvas) {
+        scenes.get(ACTIVE_SCENE).draw(canvas);
+    }
+}
