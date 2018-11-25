b75346e58b7b5807c3f011e2eab91dd38ba19430
chrison <chris.on@mail.utoronto.ca>
2018-11-25 18:29:19 -0500

    Added activity parameter to initializer

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/UltimateTTT/UltimateTTTBackend.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/UltimateTTT/UltimateTTTBackend.java
@@ -4,14 +4,17 @@ class UltimateTTTBackend {
 //Adapted from: https://github.com/Prakash2403/UltimateTicTacToe/blob/master/app/src/main/java/com/example/prakash/ultimatetictactoe/backend/Backend.java
 
     UltTTTBackendExecute executer;
+    UltimateTTTGameActivity activity;
+    UltTTTGameStateScanner scanner;
 
-    UltimateTTTBackend() {
+    UltimateTTTBackend(UltimateTTTGameActivity activity) {
         UltTTTBackendInit initializer = new UltTTTBackendInit();
         initializer.initialize();
 
-        UltTTTGameStateScanner scanner = new UltTTTGameStateScanner(initializer);
+        scanner = new UltTTTGameStateScanner(initializer, activity);
         UltTTTGameStates gamestates = new UltTTTGameStates(initializer, scanner);
         executer = new UltTTTBackendExecute(initializer, gamestates);
+        this.activity = activity;
     }
 }
 
