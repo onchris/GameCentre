d1076f066c5ab309359cf02f74335f1ba3b7ad76
likevi23 <kevinbobby.li@mail.utoronto.ca>
2018-11-28 01:30:06 -0500

    Finished Instrumentation Test for 3x3,4x4,5x5 with thorough checkings

--- Phase2/GameCentre/app/src/androidTest/java/fall2018/csc2017/slidingtiles/GameSelectionTest.java
+++ Phase2/GameCentre/app/src/androidTest/java/fall2018/csc2017/slidingtiles/GameSelectionTest.java
@@ -1,6 +1,12 @@
 package fall2018.csc2017.slidingtiles;
 
+import android.accessibilityservice.AccessibilityService;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.os.IBinder;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.IdlingPolicies;
@@ -9,11 +15,14 @@ import android.support.test.espresso.IdlingResource;
 import android.support.test.espresso.PerformException;
 import android.support.test.espresso.UiController;
 import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.action.EspressoKey;
 import android.support.test.espresso.action.KeyEventAction;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.base.IdlingResourceRegistry;
 import android.support.test.espresso.idling.CountingIdlingResource;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.espresso.matcher.BoundedMatcher;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.espresso.util.HumanReadables;
 import android.support.test.espresso.util.TreeIterables;
@@ -21,9 +30,12 @@ import android.support.test.filters.SmallTest;
 import android.util.Log;
 import android.view.SurfaceView;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.After;
@@ -54,16 +66,22 @@ import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.Intents.intending;
 import static android.support.test.espresso.intent.Intents.times;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.RootMatchers.isDialog;
+import static android.support.test.espresso.matcher.RootMatchers.isPlatformPopup;
+import static android.support.test.espresso.matcher.RootMatchers.isSystemAlertWindow;
 import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
 import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withResourceName;
+import static android.support.test.espresso.matcher.ViewMatchers.withTagValue;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.anyOf;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.hasToString;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.startsWith;
@@ -79,7 +97,46 @@ public class GameSelectionTest {
             super.beforeActivityLaunched();
         }
     };
+    /**
+     * Custom matcher for comparing drawables of a view component, useful for debugging correct
+     * tile digits being displayed
+     * @param targetBackground the background to be matched against
+     * @return A valid view matcher for check assertions
+     */
+    private static Matcher<View> checkTargetBackground(final Drawable targetBackground){
+        return new BoundedMatcher<View, Button>(Button.class) {
+            @Override
+            protected boolean matchesSafely(Button item) {
+                LayerDrawable convertedLayers = (LayerDrawable) item.getBackground();
+                boolean matching = false;
+                Bitmap targetBitmap = getBitmap(targetBackground);
+                for(int layer = 0; layer < convertedLayers.getNumberOfLayers(); layer++){
+                    if(convertedLayers.getDrawable(layer).getClass() == targetBackground.getClass()) {
+                        Bitmap comparisonBitmap = getBitmap(convertedLayers.getDrawable(layer));
+                        matching = comparisonBitmap.sameAs(targetBitmap);
+                        if(matching)
+                            return true;
+                    }
+                }
+                return matching;
+            }
 
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Should match specific drawable background")
+                        .appendText(targetBackground.toString());
+            }
+
+            private Bitmap getBitmap(Drawable drawable) {
+                Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
+                        drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+                Canvas canvas = new Canvas(bitmap);
+                drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+                drawable.draw(canvas);
+                return bitmap;
+            }
+        };
+    }
     @Test
     public void test1_testSelectionGames() throws Throwable {
         Intent intent = new Intent();
@@ -152,6 +209,60 @@ public class GameSelectionTest {
     }
     @Test
     public void test3_testSlidingTileCreation(){
+        Intent intent = new Intent();
+        intent.putExtra("currentUser", "123");
+        testRule.launchActivity(intent);
+        onView(withId(R.id.text_loggedas)).check(matches(withText("123")));
+        onView(withId(R.id.button_gameselect1)).perform(click());
+        onView(withId(R.id.scrollable_loadablegames)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_slidingreset)).check(matches(isDisplayed())).perform(click());
+        onView(withText("Delete all games?")).inRoot(isDialog()).check(matches(isDisplayed()));
+        onView(allOf(withId(android.R.id.button1))).perform(click());
+        onView(withId(R.id.button_slidingreset)).check(matches(isDisplayed())).perform(click());
+        onView(withText("Delete all games?")).inRoot(isDialog()).check(matches(isDisplayed()));
+        onView(allOf(withId(android.R.id.button1))).perform(click());
+
+        //InstrumentationRegistry.getInstrumentation().getUiAutomation().performGlobalAction(AccessibilityService.GLOBAL_ACTION_BACK);
+        //onView(withText("YES")).inRoot(isSystemAlertWindow()).check(matches(isDisplayed())).perform(click());
+        onView(withId(R.id.button_newgame)).check(matches(isDisplayed())).perform(click());
+        onView(withText("3x3")).inRoot(isPlatformPopup()).check(matches(isDisplayed())).perform(click());
+        onView(allOf(withText("Load Game"), instanceOf(Button.class))).check(matches(isDisplayed())).perform(click());
+        intended(hasComponent(GameActivity.class.getName()));
+        onData(withTagValue(is((Object) 1))).atPosition(0)
+                .check(matches(checkTargetBackground(testRule.getActivity().getDrawable(R.drawable.bg_simplebg))))
+                .check(matches(checkTargetBackground(testRule.getActivity().getDrawable(R.drawable.ic_1))));
+        Espresso.pressBack();
+        onView(withId(R.id.button_gameselect1)).perform(click());
+        onView(withId(R.id.button_slidingreset)).check(matches(isDisplayed())).perform(click());
+        onView(withText("Delete all games?")).inRoot(isDialog()).check(matches(isDisplayed()));
+        onView(allOf(withId(android.R.id.button1))).perform(click());
+        onView(withId(R.id.button_newgame)).check(matches(isDisplayed())).perform(click());
+        onView(withText("4x4")).inRoot(isPlatformPopup()).check(matches(isDisplayed())).perform(click());
+        onView(allOf(withText("Load Game"), instanceOf(Button.class))).check(matches(isDisplayed())).perform(click());
+        intended(hasComponent(GameActivity.class.getName()),times(2));
+        onData(withTagValue(is((Object) 1))).atPosition(0)
+                .check(matches(checkTargetBackground(testRule.getActivity().getDrawable(R.drawable.bg_simplebg))))
+                .check(matches(checkTargetBackground(testRule.getActivity().getDrawable(R.drawable.ic_1))));
+        Espresso.pressBack();
+        onView(withId(R.id.button_gameselect1)).perform(click());
+        onView(withId(R.id.button_slidingreset)).check(matches(isDisplayed())).perform(click());
+        onView(withText("Delete all games?")).inRoot(isDialog()).check(matches(isDisplayed()));
+        onView(allOf(withId(android.R.id.button1))).perform(click());
+        onView(withId(R.id.button_newgame)).check(matches(isDisplayed())).perform(click());
+        onView(withText("5x5")).inRoot(isPlatformPopup()).check(matches(isDisplayed())).perform(click());
+        onView(allOf(withText("Load Game"), instanceOf(Button.class))).check(matches(isDisplayed())).perform(click());
+        intended(hasComponent(GameActivity.class.getName()),times(3));
+        onData(withTagValue(is((Object) 1))).atPosition(0)
+                .check(matches(checkTargetBackground(testRule.getActivity().getDrawable(R.drawable.bg_simplebg))))
+                .check(matches(checkTargetBackground(testRule.getActivity().getDrawable(R.drawable.ic_1))));
+        Espresso.pressBack();
+
+
+        //Other...
+        //onData(instanceOf(String.class)).inAdapterView(withId(R.id.scrollable_loadablegames)).atPosition(0).perform(click());
+        // onData(hasToString(startsWith("Game 0"))).usingAdapterViewProtocol(new AdapterVi).check(matches(isDisplayed()));
+        //onView(withTagValue(is((Object) LoaderAdapter.class))).check(matches(isDisplayed()));
+        //onData(instanceOf(AdapterView.class)).check(matches(isDisplayed()));
 
     }
 }
