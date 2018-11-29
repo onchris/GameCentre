c1199820512967f53755be5a9d66dd5c48143feb
chrison <chris.on@mail.utoronto.ca>
2018-11-29 14:55:38 -0500

    If current account has a save file, it will load that onCreate

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/UltimateTTT/UltimateTTTGameActivity.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/UltimateTTT/UltimateTTTGameActivity.java
@@ -12,14 +12,9 @@ import android.widget.TableLayout;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 import fall2018.csc2017.slidingtiles.Account;
-import fall2018.csc2017.slidingtiles.GameSelection;
 import fall2018.csc2017.slidingtiles.R;
-
-import fall2018.csc2017.slidingtiles.UtilityManager;
 
 /**
  * Adapted from: https://github.com/Prakash2403/UltimateTicTacToe/blob/master/app/src/main/java/com/example/prakash/ultimatetictactoe/frontend/Fifth.java
@@ -45,8 +40,6 @@ public class UltimateTTTGameActivity extends AppCompatActivity implements View.O
     /**
      * The current account
      */
-//    private List<UltTTTConnector> ultTTTBoardManagers = new ArrayList<>();
-//    private int boardNum; //TODO: remove?
 
     private Account currentAccount;
     /**
@@ -77,35 +70,23 @@ public class UltimateTTTGameActivity extends AppCompatActivity implements View.O
             currentAccount = (Account) getIntent().getSerializableExtra("account");
             P1Name = currentAccount.getUsername();
             P2Name = "Guest";
-        } else {
-            IS_GUEST = true;
-            P1Name = "Guest1";
-            P2Name = "Guest2";
+            if (currentAccount.getUltimateTTTSave().size() != 0) {
+                ArrayList saveFile = currentAccount.getUltimateTTTSave();
+                connector = new UltTTTConnector(this);
+                ImageButtons = connector.getImageButtons();
+                tables = connector.getTables();
+                initialize();
+                runFrontEnd(saveFile);
+            } else {
+                IS_GUEST = true;
+                P1Name = "Guest1";
+                P2Name = "Guest2";
+                connector = new UltTTTConnector(this);
+                ImageButtons = connector.getImageButtons();
+                tables = connector.getTables();
+                initialize();
+            }
         }
-
-        connector = new UltTTTConnector(this );
-        ImageButtons = connector.getImageButtons();
-        tables = connector.getTables();
-        initialize();
-
-//        try {
-//            ultTTTBoardManagers = currentAccount.getUltimateTTTList();
-//            boardNum = getIntent().getIntExtra("ultTTTBoardNum", ultTTTBoardManagers.size() - 1);
-//            if (boardNum > -1) {
-////                ultTTTBoard = ultTTTBoardManagers.remove(boardNum);
-////                ultTTTBoardManager = new UltTTTBoardManager(ultTTTBoard, connector);
-//                connector = new UltTTTConnector(this, ultTTTBoardManagers.remove(boardNum).getBackend());
-//            }
-//            ImageButtons = connector.getImageButtons();
-//            tables = connector.getTables();
-//            initialize(connector);
-//        } catch (Exception e) {
-//            connector = new UltTTTConnector(this);
-//            ImageButtons = connector.getImageButtons();
-//            tables = connector.getTables();
-//            initialize();
-//            e.printStackTrace();
-//        }
     }
 
     /**
@@ -126,22 +107,6 @@ public class UltimateTTTGameActivity extends AppCompatActivity implements View.O
         connector.bundo.setEnabled(true);
     }
 
-//    private void initialize(UltTTTConnector connector) {
-//        //TODO: may not need this anymore
-//
-//        for (ImageButton ImageButton : connector.getImageButtons()) {
-//            ImageButton.setOnClickListener(this);
-//            ImageButton.setBackgroundResource(R.drawable.ult_clearimage);
-//        }
-//        for (TableLayout tableLayout : connector.getTables())
-//            tableLayout.setBackgroundColor(Color.BLACK);
-//        connector.breset.setOnClickListener(this);
-//        connector.breset.setEnabled(true);
-//        connector.bundo.setOnClickListener(this);
-//        connector.bundo.setEnabled(true);
-//    }
-
-
     @Override
     public void onClick(View v) {
         int index;
@@ -160,34 +125,21 @@ public class UltimateTTTGameActivity extends AppCompatActivity implements View.O
      */
     public void runFrontEnd(int index) {
         JSONObject response;
-        if (GameSelection.IS_GUEST) {
-            response = connector.backend.executer.execute(index);
-        } else {
-            response = UltimateTTTInfoManager.getMapToJson(currentAccount.getUltimateTTTSave());
-        }
+        response = connector.backend.executer.execute(index);
         ultTTTBoardManager = new UltTTTBoardManager(UltimateTTTInfoManager.parseJson(response), connector);
-        if (!IS_GUEST) {
-            UtilityManager.saveUltTTTBoardManager(this, currentAccount,currentAccount.getUltimateTTTSave() );
-//            if (boardNum < 0) {
-//                ultTTTBoardManagers.add(connector);
-//                boardNum ++;
-//            } else {
-//                ultTTTBoardManagers.add(boardNum, connector);
-//                UtilityManager.saveUltTTTBoardManager(this, currentAccount, ultTTTBoardManagers);
-//            }
+    }
+
+    public void runFrontEnd(ArrayList<Integer> index) {
+        JSONObject response;
+        for (int i : index) {
+            response = connector.backend.executer.execute(i);
+            ultTTTBoardManager = new UltTTTBoardManager(UltimateTTTInfoManager.parseJson(response), connector);
+            ultTTTBoardManager.operate();
         }
     }
 
     @Override
     public void onBackPressed() {
-//        if (!IS_GUEST) {
-//            if (ultTTTBoardManagers.size() == 0) {
-//                ultTTTBoardManagers.add(connector);
-//            } else {
-//                ultTTTBoardManagers.add(boardNum, connector);
-//                UtilityManager.saveUltTTTBoardManager(this, currentAccount, ultTTTBoardManagers);
-//            }
-//        }
         super.onBackPressed();
         finish();
     }
