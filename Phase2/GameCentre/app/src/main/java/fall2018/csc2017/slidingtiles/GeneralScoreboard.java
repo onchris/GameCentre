9720e2fb4ec509f4fd7ba921b4d42c8ccb586d50
likevi23 <kevinbobby.li@mail.utoronto.ca>
2018-11-29 18:56:59 -0500

    GeneralScoreboard now allows changing scores between user-specific scores and global scores that shares among all users

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/GeneralScoreboard.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/GeneralScoreboard.java
@@ -8,29 +8,47 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-public class GeneralScoreboard extends AppCompatActivity {
+import static fall2018.csc2017.slidingtiles.UtilityManager.loadAccountList;
+import static fall2018.csc2017.slidingtiles.UtilityManager.makeCustomToastText;
+
+public class GeneralScoreboard extends AppCompatActivity implements ScoreFragment.OnFragmentInteractionListener {
     private ViewPager viewPager;
     private FragmentPagerAdapter fragmentPagerAdapter;
     private String username;
     private TextView displayUsername;
-
+    private Fragment currentFragment;
+    private ListView currentScoreDisplay;
+    private boolean isGuest;
+    private boolean isGlobalScoreboard;
+    private Account currentAccount;
+    private List<String> displayUserScoresList, defaultList;
+    private ScoreManager fragmentScoreManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_viewpager_scores);
-        viewPager = findViewById(R.id.vp_scores);
         username = getIntent().getStringExtra("username");
+        currentAccount = username == null ? null : new AccountManager(loadAccountList(this)).getAccountFromUsername(username);
+        isGuest = currentAccount == null;
+        viewPager = findViewById(R.id.vp_scores);
         displayUsername = findViewById(R.id.text_generalscore_user);
         displayUsername.setText(
                 getString(R.string.gsb_logged_as, username == null ? "Guest" : username));
         fragmentPagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(fragmentPagerAdapter);
+        isGlobalScoreboard = true;
     }
 
     @Override
@@ -39,7 +57,37 @@ public class GeneralScoreboard extends AppCompatActivity {
         getSupportFragmentManager().popBackStack();;
     }
 
+    public void backgroundImageViewOnClick(View view){
+        if (isGuest) {
+            makeCustomToastText(getString(R.string.gsb_no_score), this);
+            return;
+        }
+        getDisplayScoreboard();
+        fragmentScoreManager = ((ScoreFragment) currentFragment).getScoreManager();
+        displayUserScoresList = fragmentScoreManager.getDisplayUserScoresList();
+        defaultList = fragmentScoreManager.getDisplayGameScoresList();
+        if (isGlobalScoreboard) {
+            ArrayAdapter arrayAdapter = new ArrayAdapter<>(this, R.layout.activity_scorelist, displayUserScoresList);
+            currentScoreDisplay.setAdapter(arrayAdapter);
+            arrayAdapter.notifyDataSetChanged();
+            isGlobalScoreboard = !isGlobalScoreboard;
+        }
+        else {
+            ArrayAdapter arrayAdapter = new ArrayAdapter<>(this,
+                R.layout.activity_scorelist, defaultList);
+            currentScoreDisplay.setAdapter(arrayAdapter);
+            arrayAdapter.notifyDataSetChanged();
+            isGlobalScoreboard = !isGlobalScoreboard;
+        }
+    }
 
+    @Override
+    public void getDisplayScoreboard() {
+        currentFragment = fragmentPagerAdapter.getItem(viewPager.getCurrentItem());
+        if (currentFragment instanceof ScoreFragment){
+            currentScoreDisplay = ((ScoreFragment) currentFragment).getScoresListDisplay();
+        }
+    }
 
     public class FragmentPagerAdapter extends FragmentStatePagerAdapter{
         private List<Fragment> fragments = new ArrayList<>();
