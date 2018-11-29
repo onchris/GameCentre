9ea329505e2b3513f53136bb00e0ba1751cf572d
chrison <chris.on@mail.utoronto.ca>
2018-11-29 18:48:17 -0500

    Added java docs for GameObject class

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/ObstacleDodger/GameObject.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/ObstacleDodger/GameObject.java
@@ -8,6 +8,14 @@ https://www.youtube.com/watch?v=OojQitoAEXs - Retro Chicken Android Studio 2D Ga
 import android.graphics.Canvas;
 
 public interface GameObject {
+
+    /*
+    Draws a game object.
+     */
     void draw(Canvas canvas);
+
+    /*
+    Updates a game object.
+     */
     void update();
 }
