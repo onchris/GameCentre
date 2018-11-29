39994ba48aa1ecc08f2582d643833e1976651f93
chrison <chris.on@mail.utoronto.ca>
2018-11-29 18:40:47 -0500

    Fixed android warnings

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/ObstacleDodger/Animation.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/ObstacleDodger/Animation.java
@@ -17,17 +17,17 @@ public class Animation {
 
     private boolean isPlaying = false;
 
-    public boolean isPlaying() {
+    boolean isPlaying() {
         return isPlaying;
     }
 
-    public void play() {
+    void play() {
         isPlaying = true;
         frameIndex = 0;
         lastFrame = System.currentTimeMillis();
     }
 
-    public void stop() {
+    void stop() {
         isPlaying = false;
     }
 
@@ -35,7 +35,7 @@ public class Animation {
 
     private long lastFrame;
 
-    public Animation(Bitmap[] frames, float animTime) {
+    Animation(Bitmap[] frames, float animTime) {
         this.frames = frames;
         frameIndex = 0;
 
