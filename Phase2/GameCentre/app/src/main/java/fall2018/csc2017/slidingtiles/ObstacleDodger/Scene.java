2cd30b2c894ca5fb8cf0b1c6bcfa58664abc4290
linshixi <shixiu.lin@mail.utoronto.ca>
2018-11-29 23:21:20 -0500

    Added Javadocs for obstacle dodger game scene class

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/ObstacleDodger/Scene.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/ObstacleDodger/Scene.java
@@ -8,12 +8,31 @@ https://www.youtube.com/watch?v=OojQitoAEXs - Retro Chicken Android Studio 2D Ga
 import android.graphics.Canvas;
 import android.view.MotionEvent;
 
+/**
+ * Interface for Obstacle Dodger game play scene
+ */
 public interface Scene {
+    /**
+     * Updates a scene
+     */
     public void update();
 
+    /**
+     * Draws a game scene.
+     *
+     * @param canvas the canvas to be drawn.
+     */
     public void draw(Canvas canvas);
 
+    /**
+     * To terminate a scene
+     */
     public void terminate();
 
+    /**
+     * Receive the touch from player
+     *
+     * @param event the motion taken by player
+     */
     public void receiveTouch(MotionEvent event);
 }
