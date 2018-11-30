0e8dfb5cf6ece3ee1e55e76ff0e5e5fd956434ea
chrison <chris.on@mail.utoronto.ca>
2018-11-29 19:23:40 -0500

    fixed android warnings and added java docs for ObDodgerObserver class

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/ObstacleDodger/ObDodgerObserver.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/ObstacleDodger/ObDodgerObserver.java
@@ -1,40 +1,44 @@
 package fall2018.csc2017.slidingtiles.ObstacleDodger;
 
-import android.app.Activity;
-import android.app.ActivityManager;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.WindowManager;
 
-import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
 import fall2018.csc2017.slidingtiles.Account;
-import fall2018.csc2017.slidingtiles.ScoreBoard;
 
 import static fall2018.csc2017.slidingtiles.UtilityManager.saveObDodgerScoresToAccounts;
 
+/*
+Class to observe whether the obstacle dodger game is over.
+ */
 public class ObDodgerObserver implements Observer {
 
-    ObGameActivity activity;
-    GamePanel gamePanel;
-    Account currentAccount;
+    /*
+    The game activity of the observer.
+     */
+    private ObGameActivity activity;
 
+    /*
+    The game panel of the observer.
+     */
+    private GamePanel gamePanel;
+
+    /*
+    The current account of the observer.
+     */
+    private Account currentAccount;
+
+    /*
+    Creates a new observer.
+     */
     ObDodgerObserver(GamePanel gamePanel, Account account, ObGameActivity activity) {
         this.gamePanel = gamePanel;
         this.currentAccount = account;
         this.activity = activity;
     }
 
-
     @Override
-    /**
-     * On notified GameplayScene has changed, save score to account and change to scoreboard
-     */
     public void update(Observable o, Object arg) {
         Integer score = (Integer) arg;
         Bundle infoBundle = new Bundle();
@@ -51,6 +55,5 @@ public class ObDodgerObserver implements Observer {
         o.deleteObservers();
         gamePanel.surfaceDestroyed(gamePanel.getHolder());
         activity.createScoreBoard(score, infoBundle);
-
     }
 }
