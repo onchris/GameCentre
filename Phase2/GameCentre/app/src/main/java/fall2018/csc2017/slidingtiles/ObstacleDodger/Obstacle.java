6b0614995395d93a48b0bb2c3ff490254cff54e8
linshixi <shixiu.lin@mail.utoronto.ca>
2018-11-29 22:47:44 -0500

    Added Javadocs for obstacle

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/ObstacleDodger/Obstacle.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/ObstacleDodger/Obstacle.java
@@ -1,5 +1,4 @@
 package fall2018.csc2017.slidingtiles.ObstacleDodger;
-
 /*
 Adapted from:
 https://www.youtube.com/watch?v=OojQitoAEXs - Retro Chicken Android Studio 2D Game Series
@@ -9,16 +8,37 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
 
+/**
+ * The obstacle
+ */
 public class Obstacle implements GameObject {
-
+    /**
+     * The rectangle that is one part of obstacle
+     */
     private Rect rectangle;
+    /**
+     * The rectangle that is the other part of obstacle
+     */
     private Rect rectangle2;
+    /**
+     * The color of obstacle
+     */
     private int color;
 
+    /**
+     * Gets a rectangle
+     *
+     * @return the rectangle
+     */
     public Rect getRectangle() {
         return rectangle;
     }
 
+    /**
+     * Increases obstacle's size by y
+     *
+     * @param y the amount to be added to obstacle
+     */
     public void incrementY(float y) {
         rectangle.top += y;
         rectangle.bottom += y;
@@ -26,12 +46,27 @@ public class Obstacle implements GameObject {
         rectangle2.bottom += y;
     }
 
+    /**
+     * The obstacle
+     *
+     * @param rectHeight the height of obstacle
+     * @param color      the color of obstacle
+     * @param startX     the start point x
+     * @param startY     the start point y
+     * @param playerGap  the gap for player to pass through
+     */
     public Obstacle(int rectHeight, int color, int startX, int startY, int playerGap) {
         this.color = color;
         rectangle = new Rect(0, startY, startX, startY + rectHeight);
         rectangle2 = new Rect(startX + playerGap, startY, ObUtilityManager.getScreenWidth(), startY + rectHeight);
     }
 
+    /**
+     * Check if the player collides with obstacle
+     *
+     * @param player the player
+     * @return if the player collides with obstacle
+     */
     public boolean playerCollide(RectPlayer player) {
         return Rect.intersects(rectangle, player.getRectangle()) || Rect.intersects(rectangle2, player.getRectangle());
     }
@@ -46,6 +81,5 @@ public class Obstacle implements GameObject {
 
     @Override
     public void update() {
-
     }
 }
