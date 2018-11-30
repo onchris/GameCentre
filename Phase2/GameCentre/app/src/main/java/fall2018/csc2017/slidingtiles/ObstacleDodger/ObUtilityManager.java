553fedd1eff25ee9e35bc7936a0cfe01d8f6ee31
linshixi <shixiu.lin@mail.utoronto.ca>
2018-11-29 22:50:40 -0500

    Added Javadocs for obstacleUtilityManager

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/ObstacleDodger/ObUtilityManager.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/ObstacleDodger/ObUtilityManager.java
@@ -1,5 +1,4 @@
 package fall2018.csc2017.slidingtiles.ObstacleDodger;
-
 /*
 Adapted from:
 https://www.youtube.com/watch?v=OojQitoAEXs - Retro Chicken Android Studio 2D Game Series
@@ -8,14 +7,29 @@ https://www.youtube.com/watch?v=OojQitoAEXs - Retro Chicken Android Studio 2D Ga
 import android.content.Context;
 import android.content.res.Resources;
 
+/**
+ * Manage obstacle utility, including get the height and width of screen
+ */
 public class ObUtilityManager {
-
+    /**
+     * The current context
+     */
     public static Context CURRENT_CONTEXT;
 
+    /**
+     * Gets the screen width
+     *
+     * @return the screen width
+     */
     public static int getScreenWidth() {
         return Resources.getSystem().getDisplayMetrics().widthPixels;
     }
 
+    /**
+     * Gets the screen height
+     *
+     * @return the screen height
+     */
     public static int getScreenHeight() {
         return Resources.getSystem().getDisplayMetrics().heightPixels;
     }
